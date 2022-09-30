d75e5f03c2ec5ccc73b9bf61f293ed768461deef
René Jochum <rene@jochum.dev>
2022-09-30 04:36:55 +0200

    Improve natsnoflags

--- natsnoflags/main.go
+++ natsnoflags/main.go
@@ -4,25 +4,36 @@ import (
 	"go-micro.dev/v4/logger"
 
 	"github.com/go-micro/microwire"
-	"github.com/go-micro/microwire/broker"
 	_ "github.com/go-micro/microwire/plugins/cli/urfave"
-	"github.com/go-micro/microwire/registry"
-	"github.com/go-micro/microwire/transport"
 	_ "github.com/go-micro/plugins/v4/broker/nats"
 	_ "github.com/go-micro/plugins/v4/registry/nats"
 	_ "github.com/go-micro/plugins/v4/transport/nats"
 )
 
 func main() {
-	service, err := microwire.NewService(
+	configStore, err := microwire.NewConfigStore()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	broker := configStore.GetBroker()
+	broker.Plugin = "nats"
+	broker.Addresses = []string{"nats://localhost:4222"}
+
+	registry := configStore.GetRegistry()
+	registry.Plugin = "nats"
+	registry.Addresses = []string{"nats://localhost:4222"}
+
+	transport := configStore.GetTransport()
+	transport.Plugin = "nats"
+	transport.Addresses = []string{"nats://localhost:4222"}
+
+	service, err := microwire.NewServiceWithConfigStore(
+		configStore,
 		microwire.Name("natsnoflags"),
 		microwire.Usage("A POC for go-micro.dev/v5"),
 		microwire.Version("v0.0.1"),
 		microwire.NoFlags(),
-
-		microwire.Component(broker.ComponentName, "nats"),
-		microwire.Component(registry.ComponentName, "nats"),
-		microwire.Component(transport.ComponentName, "nats"),
 	)
 	if err != nil {
 		logger.Fatal(err)
